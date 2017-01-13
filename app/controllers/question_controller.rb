d24474f69333f627582a82d0f92f9a2f951cef6a
Hesham Elrehaimy & Anna Pougas <pair+hesham-elrehaimy.karyatis@devbootcamp.com>
2017-01-13 08:34:21 -0600

    Can post questions

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,5 +1,23 @@
+get '/questions/new' do 
+
+	erb :"/questions/new"
+end
+
+post '/questions' do 
+  @question = Question.new(params[:question])
+
+  if @question.save 
+
+    redirect "/questions/#{@question.id}"
+  else
+  	@errors = @question.errors.full_messages
+
+    erb :'questions/new'
+  end
+end
+
 get '/questions/:id' do
-	@question = Question.find(params[:id])
+	@question = Question.find_by(id: params[:id])
 	
 	erb :"/questions/show"
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+	validates :title, :body, presence: true
+
   belongs_to :user
   has_many :answers
   has_many :votes, :as => :voteable
